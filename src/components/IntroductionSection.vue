63a8fd89c476c74e425c2a71f43cd2eb35fa20a2
Zenara12 <nolipernes@gmail.com>
2025-02-13 03:46:39 +0800

    update:profile pic url

--- src/components/IntroductionSection.vue
+++ src/components/IntroductionSection.vue
@@ -2,7 +2,7 @@
   <q-card class="row lt-sm flex flex-center bg-transparent" flat>
     <q-avatar size="15rem" :ratio="1" outline>
       <q-img
-        src="/images/profilepic.jpg"
+        :src="imgURL"
         :ratio="1"
         spinner-color="secondary"
         class="border-2 border-info rounded-xl shadow-2"
@@ -24,7 +24,7 @@
     <q-card-section class="col gt-xs flex items-end justify-end">
       <q-avatar size="15rem" :ratio="1" outline>
         <q-img
-          src="/images/profilepic.jpg"
+          :src="imgURL"
           :ratio="1"
           spinner-color="secondary"
           loading="lazy"
@@ -40,6 +40,8 @@ import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
 
+const imgURL =
+  'https://scontent.fceb3-1.fna.fbcdn.net/v/t39.30808-6/338542128_1385363575631150_4833880374459936280_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHDVo0kHfNnq5cEo1KAeeVhesMr7Usy4NR6wyvtSzLg1Bex3TiJGg6hl-psuW-iunDJyilXolvwiYznrXRMHRYx&_nc_ohc=Gvmx35O4hHAQ7kNvgEGEK4C&_nc_oc=AdjxHLW2GjTLt1ghtBJsKE0myLvdWqoelYaVIPc4pDgwu5q18ab6ive8xS_EhyGjQL8&_nc_zt=23&_nc_ht=scontent.fceb3-1.fna&_nc_gid=AdPhE7zsqWUfDpHkihFZV4e&oh=00_AYA_7yWvAI6lfYwnDHUcelDyvuu393EhMvz_ui1sEhZkXg&oe=67B2DEDB'
 const myName = 'Noli'
 const introduction =
   'I am a starting web developer with a passion for building user-friendly websites and applications. Skilled in VueJS, JavaScript, PHP, and database management, I have worked on freelance projects that involve front-end development, backend integration, and troubleshooting. I am eager to learn, adapt, and grow in the field of web development, always looking for new challenges to improve my skills and create efficient digital solutions.'
